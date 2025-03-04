a7c93e5a10ea0cfc9e20114d2297f23bb3d00f58
潘郁婷 <panyuting@panyutingdeMacBook-Air.local>
2025-03-04 16:17:42 +0800

    got 50 percent correct on this version

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,20 +3,21 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  def validate_checksum(number)
+  def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
     sum = 0
-    parity = nums_a.length % 2
-    nums_a.each_with_index |n,i| do 
+    parity = (nums_a.length - 1) % 2
+    nums_a[0...-1].each_with_index do |n,i|
       if i % 2 != parity
-        sum = sum + n
+        sum += n
       elsif n > 4
-        sum = sum + 2 * n - 9
+        sum = sum + (2*n -9) 
       else
-        sum = sum + 2 * n
+        sum = sum + 2*n
       end
     end
     return nums_a[-1] == ((10 - (sum % 10)) % 10 )
     # TODO: use the integers in nums_a to validate its last check digit
   end
+  
 end
