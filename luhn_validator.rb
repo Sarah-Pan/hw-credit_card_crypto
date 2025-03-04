93e4322374f2a3b74616384f7b6b8f7bd7cc5479
Sarah Pan <126577175+Sarah-Pan@users.noreply.github.com>
2025-03-04 17:12:48 +0800

    Update luhn_validator.rb

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,23 +1,27 @@
+# frozen_string_literal: true
+
+# LuhnValidator module provides a method to validate a credit card number
+# using the Luhn algorithm.
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  def validate_checksum
+  # rubocop:disable Metrics/MethodLength
+  def validate_checksum # rubocop:disable Metrics/AbcSize
     nums_a = number.to_s.chars.map(&:to_i)
     sum = 0
-    parity = (nums_a.length - 1) % 2
-    nums_a[0...-1].each_with_index do |n,i|
-      if i % 2 != parity
-        sum += n
-      elsif n > 4
-        sum = sum + (2*n -9) 
-      else
-        sum = sum + 2*n
-      end
+    parity = nums_a.length % 2
+    nums_a[0...-1].each_with_index do |n, i|
+      sum += if i % 2 == parity
+               doubled = n * 2
+               doubled > 9 ? (doubled - 9) : doubled
+             else
+               n
+             end
     end
-    return nums_a[-1] == ((10 - (sum % 10)) % 10 )
+    nums_a[-1] == ((10 - (sum % 10)) % 10)
     # TODO: use the integers in nums_a to validate its last check digit
   end
-  
+  # rubocop:enable Metrics/MethodLength
 end
